63bf7e9db8d17bb15164c43792ece389cdd242d9
SuspiciousLightpost <s.landstra@quicknet.nl>
2022-10-05 17:05:40 +0200

    Arguments for the port and directory

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
 use std::process::Command;
+use std::process::Output;
 use std::str;
+use std::env;
 
 fn main() {
-    {
-        let mut port  = match serialport::new("COM3", 1200)
+    let args: Vec<String> = env::args().collect();
+
+    let com_port: &str = &args[1].as_str();
+    let file_dir: &str = &args[2].as_str();
+
+    reset_port(com_port);
+    
+    let Output = match Command::new("avrdude")
+        .args(["-cstk500v2", "-pm2560", "-v", "-q", "-D", &format!("-Uflash:w:{}.hex:i", file_dir), "-b115200", &format!("-P{}", com_port)])
+        .output() {
+            Ok(output) => output,
+            Err(e) => panic!("Cant program board because {e}"),
+        };
+
+    let output_string = str::from_utf8(&output.stderr).unwrap();
+    println!("{}", output_string)
+}
+
+fn reset_port(com_port: &str) {
+    let mut port  = match serialport::new(com_port, 1200)
         .stop_bits(serialport::StopBits::One)
         .data_bits(serialport::DataBits::Eight)
         .parity(serialport::Parity::None)
         .open() {
             Ok(output) => output,
-            Err(e) => panic!("Dit is cringe {e}")
+            Err(e) => panic!("Cant open the serial port because {e}")
         };
         
-        port.write_data_terminal_ready(true).unwrap();
-        port.write_data_terminal_ready(false).unwrap();
-    }
-    
-
-    let swag = match Command::new("./avrdude")
-        .args(["-cstk500v2", "-pm2560", "-v", "-q", "-D", "-Uflash:w:C:/Users/sebas/Desktop/school/MCP1/LedBlink.hex:i", "-b115200", "-PCOM3"])
-        .output() {
-            Ok(output) => output,
-            Err(e) => panic!("Dit is best wel cringe {e}"),
-        };
-
-    let cool = str::from_utf8(&swag.stderr).unwrap();
-    println!("{}", cool)
+    port.write_data_terminal_ready(true).unwrap();
+    port.write_data_terminal_ready(false).unwrap();
 }
