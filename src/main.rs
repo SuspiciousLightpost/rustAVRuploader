5cb74da3008e24731b62e9f6a706921204f6f782
SuspiciousLightpost <s.landstra@quicknet.nl>
2022-10-09 23:47:18 +0200

    Function run command added

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::env;
 
 const STANDERD_FILE_NAME: &str = "main";
 
+const DUDE: &str = "avrdude";
+const GCC: &str = "avrgcc";
+const OBJCOPY: &str = "avr-objcopy";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -18,37 +22,20 @@ fn main() {
 }
 
 fn compile_file(file_name: &str) {
-    let compile_out = match Command::new("avr-gcc")
-        .args(["-O2", "-Wall", "-mmcu=atmega2560", &format!("{}.c", file_name), "-o", &format!("{}.out", file_name)])
-        .output() {
-            Ok(compile_out) => compile_out,
-            Err(e) => panic!("Cant compile to out file because {e}"),
-        };
-    let compile_out_string = str::from_utf8(&compile_out.stderr).unwrap();
-    println!("{}", compile_out_string);
-    
-    let compile_hex = match Command::new("avr-objcopy")
-        .args(["-O", "ihex", &format!("{}.out", file_name), &format!("{}.hex", file_name)])
-        .output() {
-            Ok(compile_hex) => compile_hex,
-            Err(e) => panic!("Cant compile to out file because {e}"),
-        };
-    let compile_hex_string = str::from_utf8(&compile_hex.stderr).unwrap();
-    println!("{}", compile_hex_string);
+    /* Compiling the c file to out file */
+    let mut args = ["-O2", "-Wall", "-mmcu=atmega2560", &format!("{}.c", file_name), "-o", &format!("{}.out", file_name)];
+    run_command(GCC,&mut args);
+
+    /* Compiling the out file to hex file */
+    let mut args = ["-O", "ihex", &format!("{}.out", file_name), &format!("{}.hex", file_name)];
+    run_command(OBJCOPY, &mut args);
 }
 
 fn upload_file(file_name: &str, com_port: &str) {
     reset_port(com_port);
 
-    let output = match Command::new("avrdude")
-        .args(["-cstk500v2", "-pm2560", "-v", "-q", "-D", &format!("-Uflash:w:{}.hex:i", file_name), "-b115200", &format!("-P{}", com_port)])
-        .output() {
-            Ok(output) => output,
-            Err(e) => panic!("Cant program board because {e}"),
-        };
-
-    let output_string = str::from_utf8(&output.stderr).unwrap();
-    println!("{}", output_string)
+    let mut args = ["-cstk500v2", "-pm2560", "-v", "-q", "-D", &format!("-Uflash:w:{}.hex:i", file_name), "-b115200", &format!("-P{}", com_port)];
+    run_command(DUDE, &mut args)
 }
 
 fn reset_port(com_port: &str) {
@@ -63,4 +50,16 @@ fn reset_port(com_port: &str) {
         
     port.write_data_terminal_ready(true).unwrap();
     port.write_data_terminal_ready(false).unwrap();
+}
+
+fn run_command(program_name: &str, command_args: &mut [&str]) {
+    let output = match Command::new(program_name)
+        .args(command_args)
+        .output() {
+            Ok(output) => output,
+            Err(e) => panic!("{e}"),
+        };
+
+    let output_string = str::from_utf8(&output.stderr).unwrap();
+    println!("{}", output_string)
 }
